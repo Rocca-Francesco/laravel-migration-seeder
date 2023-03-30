8a62ab79e7e0937c57b378061856ba7ce3794f71
Rocca-Francesco <roccafrance98@yahoo.it>
2023-03-30 18:07:33 +0200

    stampa in pagina

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -10,6 +10,35 @@
   @vite('resources/js/app.js')
 </head>
 <body>
-  
+  <div class="container my-4">
+    <table class="table table-dark table-striped">
+  <thead>
+    <tr class="text-warning">
+      <th scope="col">AZIENDA</th>
+      <th scope="col">CODICE TRENO</th>
+      <th scope="col">STAZIONE DI PARTENZA</th>
+      <th scope="col">ORARIO DI PARTENZA</th>
+      <th scope="col">STATO</th>
+    </tr>
+  </thead>
+  <tbody>
+    @forelse ($trains as $train)
+    <tr>
+      <td>{{$train->azienda}}</td>
+      <td>{{$train->codice_treno}}</td>
+      <td>{{$train->stazione_partenza}}</td>
+      <td>{{$train->orario_di_partenza}}</td>
+      <td>{{$train->stato}}</td>
+    </tr>
+    @empty
+      <td>NESSUN DATO</td>
+      <td>NESSUN DATO</td>
+      <td>NESSUN DATO</td>
+      <td>NESSUN DATO</td>
+      <td>NESSUN DATO</td>
+    @endforelse
+  </tbody>
+</table>
+  </div>
 </body>
 </html>
